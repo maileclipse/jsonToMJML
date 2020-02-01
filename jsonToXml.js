b8810f13be6029b8202db2c8ce2fcae4ed1e2f75
Yassine <qoraicheofficiel@hotmail.com>
2020-02-01 17:45:34 +0100

    Create jsonToXml.js

---
+++ jsonToXml.js
@@ -0,0 +1,21 @@
+/**
+ * Source:
+ * https://github.com/mjmlio/mjml
+ */
+
+const jsonToXML = ({ tagName, attributes, children, content }) => {
+    const subNode =
+        children && children.length > 0
+            ? children.map(jsonToXML).join('\n')
+            : content || ''
+
+    const stringAttrs = Object.keys(attributes)
+        .map(attr => `${attr}="${attributes[attr]}"`)
+        .join(' ')
+
+    return `<${tagName}${stringAttrs === ''
+        ? '>'
+        : ` ${stringAttrs}>`}${subNode}</${tagName}>`
+}
+
+export default jsonToXML
